a6755ab8193e35e7512b0d9855a3541d482071f9
Andrzej Krzywda <andrzejkrzywda@gmail.com>
2014-06-06 15:24:23 +0200

    pass the test

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -4,4 +4,16 @@ class CalculatorTest < Test::Unit::TestCase
   def test_empty_string_sums_up_to_zero
     assert_equal(0, add(""))
   end
+
+  def add(expression)
+    StringCalculator.new.add(expression)
+  end
 end
+
+
+class StringCalculator
+  def add(expression)
+    0
+  end
+end
+
