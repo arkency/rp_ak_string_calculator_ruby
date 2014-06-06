b206eb1c0d72613888071c7aa50ee1a491200019
Robert Pankowecki <robert.pankowecki@gmail.com>
2014-06-06 15:27:36 +0200

    One number test passes

--- string_calculator_test.rb
+++ string_calculator_test.rb
@@ -3,6 +3,9 @@ require 'test/unit'
 class CalculatorTest < Test::Unit::TestCase
   def test_empty_string_sums_up_to_zero
     assert_equal(0, add(""))
+  end
+
+  def test_one_number_sums_up_to_the_number
     assert_equal(1, add("1"))
   end
 
@@ -14,7 +17,7 @@ end
 
 class StringCalculator
   def add(expression)
-    0
+    Integer(expression) rescue 0
   end
 end
 
